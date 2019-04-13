6d5261bcda9764936d9abc4ef5348b1990740af5
Isaac Wong <339@holbertonschool.com>
2019-04-13 03:57:08 +0000

    Cat app

---
+++ cat-app/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Cat.create(name: "Ginger", description: "An orange like cat")
+Cat.create(name: "Hobbes", description: "An Hobbes like cat")
+Cat.create(name: "Taro", description: "An spotted like cat")
+Cat.create(name: "Linda", description: "An normal like cat")
